037543be536ea4b2af659e9ce05ef97ce3a03ec3
Fest <fest.payhip@gmail.com>
2026-05-22 23:28:41 +0530

    Add files via upload

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -3,6 +3,19 @@ import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { createTicket } from '../api/client'; 
 
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
 function LocationPicker({ position, setPosition }) {
   useMapEvents({
     click(e) {
